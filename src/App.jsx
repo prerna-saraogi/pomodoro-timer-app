7efe286f27b7e90bccb75ec5b1033fecfec0c9fa
Prerna Saraogi <prernasaraogi22@gmail.com>
2025-03-03 07:55:06 +0530

    initialize vite react app with folder structure and setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center text-center">
+      <h1 className="text-2xl font-bold">Pomodoro Timer App</h1>
+    </div>
+  );
+};
+
+export default App;
